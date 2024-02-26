edf011428abe00da65ed7a06367aa3c776d4e5d8
Aline Viana <80078418+alineviana@users.noreply.github.com>
2024-02-26 13:48:06 -0300

    features

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,26 +1,49 @@
-import { Router } from '@angular/router';
-import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Router } from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { ConsultaCepService } from "../service/consulta-cep.service";
 
 @Component({
-  selector: 'app-cadastro',
-  templateUrl: './cadastro.component.html',
-  styleUrls: ['./cadastro.component.css']
+  selector: "app-cadastro",
+  templateUrl: "./cadastro.component.html",
+  styleUrls: ["./cadastro.component.css"],
 })
 export class CadastroComponent implements OnInit {
+  constructor(
+    private router: Router,
+    private consultaCepService: ConsultaCepService
+  ) { }
 
-  constructor(private router: Router) { }
+  ngOnInit(): void { }
 
-  ngOnInit(): void {
+  consultaCEP(ev: any, form: NgForm) {
+    const cep = ev.target.value;
+    if (cep !== "") {
+      this.consultaCepService.getConsultaCep(cep).subscribe((resultado) => {
+        console.log(resultado);
+        this.populandoEndereco(resultado, form);
+      });
+    }
   }
 
-  cadastrar(form: any) {
-    console.log(form);
+  populandoEndereco(dados: any, form: NgForm) {
+    console.log('Data received from API:', dados);
+    form.form.patchValue({
+      endereco: dados.logradouro,
+      complemento: dados.complemento,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    });
+    console.log('Form values after patching:', form.value);
+  }
+
+  cadastrar(form: NgForm) {
     if (form.valid) {
-      this.router.navigate(['sucesso']);
-      console.log('Formulário enviado');
+      this.router.navigate(["./sucesso"]);
     } else {
-      console.log('Formulário inválido');
+      alert("Formulário inválido");
     }
+    console.log(form.controls);
   }
 }
